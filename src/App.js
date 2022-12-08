fdbd06274eb6e0f55005cadc71ae3e023e12373c
Balaji <balaji@revidd.com>
2022-12-08 11:50:57 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and Firbase
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-3xl font-bold underline text-center mt-[10rem]  ">Abstract</h1>
+      <p className="text-justify ml-[24rem] mr-[24rem] ">
+        Understanding how software works and building code has been a basic
+        requirement for every student. Programming skills vary from person to
+        person and even from language to language. Monitoring the student
+        performance is evident to assess and improve the programming skills.
+        Though e-learning courses have paved the way to a different structure of
+        learning, it is way more important to practice under supervision. The
+        proposed model aims to automate the process of lab evaluation and
+        assessment of programming subjects in the curriculum. The basic idea is
+        to bring the IDEs of various programming languages(C, C++, Python &
+        Java) to construct a single platform to write code. The automated
+        platform consists of two types of logins. The first one is Faculty
+        login. Faculty can login to the website using the official mail ID. They
+        can add, modify or delete questions. According to the sections and
+        subjects allocated, the faculty can observe the performance trends of
+        all the students of the section in a given language. Test cases will be
+        provided for each question. The second type of login is Student login.
+        In student login, the student will be able to see all the programming
+        subjects according to the section. Percentage, grades and marks will be
+        provided based on the completion of test cases. Student profile
+        demonstrates the whole details of the student. This project is being
+        built using MERN stack web development. Resolving all the errors in
+        manual evaluation, this automated tool will be very useful in assessing
+        the students. The project will be deployed using Google Firebase. This
+        tool will establish a bias free environment during programming.
+        Keywords: Programming, web development, Google Firebase, IDE, MERN ;
+      </p>
+    </>
   );
 }
 
